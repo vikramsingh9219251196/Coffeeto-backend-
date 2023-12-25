ee34562ee6c44ec7d3bde2e8db6ef05fb9afa5eb
vikramsingh9219251196 <145076898+vikramsingh9219251196@users.noreply.github.com>
2023-12-25 18:14:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
-
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
 
 const PORT = process.env.PORT || 8080;
 
